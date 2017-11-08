14aa320ed8a8082eced8e152e91b4b750fa3b01c
Kristofer Hanes <kristoferhanes@gmail.com>
2017-11-07 16:48:45 -0800

    Initial Commit

---
+++ Swapi/Swapi.h
@@ -0,0 +1,19 @@
+//
+//  Swapi.h
+//  Swapi
+//
+//  Created by Kristofer Hanes on 2017-11-07.
+//  Copyright © 2017 Kristofer Hanes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swapi.
+FOUNDATION_EXPORT double SwapiVersionNumber;
+
+//! Project version string for Swapi.
+FOUNDATION_EXPORT const unsigned char SwapiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swapi/PublicHeader.h>
+
+
